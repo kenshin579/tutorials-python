4d74b13bc28980b1cc5c599deb696ba8c6af81cd
kenshin579 <kenshin579@hotmail.com>
2020-09-17 10:32:51 +0900

    [TPY-1] list of dict 정렬 예제 추가

--- python3/sort.py
+++ python3/sort.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
+from _operator import itemgetter
 
 
-def sort_list_by_age(student_list):
+def sort_list_by_age_using_lambda(student_list):
     print('list', student_list)
     return sorted(student_list, key=lambda k: k['age'])
+
+
+def sort_list_by_age_using_itemgetter(student_list):
+    return sorted(student_list, key=itemgetter('age'))
+
+
+def sort_list_by_age_using_cmp_lambda(student_list):
+    return sorted(student_list, cmp=lambda x, y: cmp(x['age'], y['age']))
--- python3/test_sort.py
+++ python3/test_sort.py
@@ -4,19 +4,33 @@ from python3 import sort
 
 
 class Test(TestCase):
-    def test_sort_list_by_age(self):
-        student_list = [
+    def setup(self):
+        self. student_list = [
             {'name': 'Homer', 'age': 39},
             {'name': 'Homer1', 'age': 20},
             {'name': 'Homer2', 'age': 5},
             {'name': 'Bart', 'age': 10},
         ]
 
-        sorted_list_by_age = sort.sort_list_by_age(student_list)
-        for i in range(len(sorted_list_by_age)):
+
+    def sort_list_by_age_using_lambda(self):
+
+        sorted_list_by_age = sort.sort_list_by_age_using_lambda(self.student_list)
+        for i in range(len(sorted_list_by_age) - 1):
             print(i, sorted_list_by_age[i].get('age'))
-            # self.assertGreater(sorted_list_by_age[i].get('age'), sorted_list_by_age[i + 1].get('age'))
-        # for i, student in enumerate(sorted_list_by_age):
-        # print(i, student)
-        # print(sorted_list_by_age)
-        # self.assertGreater(student[i].age)
+            self.assertLess(sorted_list_by_age[i].get('age'), sorted_list_by_age[i + 1].get('age'))
+
+
+    def sort_list_by_age_using_itemgetter(self):
+
+        sorted_list_by_age = sort.sort_list_by_age_using_itemgetter(self.student_list)
+        for i in range(len(sorted_list_by_age) - 1):
+            print(i, sorted_list_by_age[i].get('age'))
+            self.assertLess(sorted_list_by_age[i].get('age'), sorted_list_by_age[i + 1].get('age'))
+
+    def sort_list_by_age_using_cmp_lambda(self):
+
+        sorted_list_by_age = sort.sort_list_by_age_using_cmp_lambda(self.student_list)
+        for i in range(len(sorted_list_by_age) - 1):
+            print(i, sorted_list_by_age[i].get('age'))
+            self.assertLess(sorted_list_by_age[i].get('age'), sorted_list_by_age[i + 1].get('age'))
