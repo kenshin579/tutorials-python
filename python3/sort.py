5e61d41040c25b71166c830a5814818c9fd7fff0
kenshin579 <kenshin579@hotmail.com>
2020-09-17 09:58:34 +0900

    [TPY-1] list of dict에서 dict의 키값으로 정렬하는 예제 작성중

--- python3/sort.py
+++ python3/sort.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
 
-
 def sort_list_by_age(student_list):
     print('list', student_list)
-    print('test')
-
+    return sorted(student_list, key=lambda k: k['age'])
--- python3/test_sort.py
+++ python3/test_sort.py
@@ -1,11 +1,22 @@
 from unittest import TestCase
 
+from python3 import sort
+
 
 class Test(TestCase):
     def test_sort_list_by_age(self):
         student_list = [
             {'name': 'Homer', 'age': 39},
-            {'name': 'Bart', 'age': 10}
+            {'name': 'Homer1', 'age': 20},
+            {'name': 'Homer2', 'age': 5},
+            {'name': 'Bart', 'age': 10},
         ]
-        sort.sort_list_by_age(student_list)
-        # self.fail()
+
+        sorted_list_by_age = sort.sort_list_by_age(student_list)
+        for i in range(len(sorted_list_by_age)):
+            print(i, sorted_list_by_age[i].get('age'))
+            # self.assertGreater(sorted_list_by_age[i].get('age'), sorted_list_by_age[i + 1].get('age'))
+        # for i, student in enumerate(sorted_list_by_age):
+        # print(i, student)
+        # print(sorted_list_by_age)
+        # self.assertGreater(student[i].age)
