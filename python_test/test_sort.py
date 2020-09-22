6c85e3f9aa42dba6e0298982fc6d282cec6673fc
kenshin579 <kenshin579@hotmail.com>
2020-09-22 18:37:07 +0900

    [TPY-3] intellij에서 실행 안되는 unit test 수정함

--- python_test/test_sort.py
+++ python_test/test_sort.py
@@ -5,21 +5,18 @@ from python_test import sort
 
 
 class SortTest(unittest.TestCase):
+    student_list = [
+        {'name': 'Homer', 'age': 39},
+        {'name': 'Homer1', 'age': 20},
+        {'name': 'Homer2', 'age': 5},
+        {'name': 'Bart', 'age': 10},
+    ]
+
     def setup(self):
-        print('setup!!!')
-        # super(SortTest, self).setUp()
-        self.student_list = [
-            {'name': 'Homer', 'age': 39},
-            {'name': 'Homer1', 'age': 20},
-            {'name': 'Homer2', 'age': 5},
-            {'name': 'Bart', 'age': 10},
-        ]
+        print('setUp is called')
 
 
     def test_sort_list_by_age_using_lambda(self):
-        print('self.student_list', self.student_list[0])
-        print('self.student_list', self.student_list)
-
         sorted_list_by_age = sort.sort_list_by_age_using_lambda(self.student_list)
         for i in range(len(sorted_list_by_age) - 1):
             print(i, sorted_list_by_age[i].get('age'))
@@ -27,14 +24,12 @@ class SortTest(unittest.TestCase):
 
 
     def test_sort_list_by_age_using_itemgetter(self):
-
         sorted_list_by_age = sort.sort_list_by_age_using_itemgetter(self.student_list)
         for i in range(len(sorted_list_by_age) - 1):
             print(i, sorted_list_by_age[i].get('age'))
             self.assertLess(sorted_list_by_age[i].get('age'), sorted_list_by_age[i + 1].get('age'))
 
     def test_sort_list_by_age_using_cmp_lambda(self):
-
         sorted_list_by_age = sort.sort_list_by_age_using_cmp_lambda(self.student_list)
         for i in range(len(sorted_list_by_age) - 1):
             print(i, sorted_list_by_age[i].get('age'))
